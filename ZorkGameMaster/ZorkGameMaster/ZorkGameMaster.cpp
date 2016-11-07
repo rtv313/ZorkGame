f6f1579aa9a37056eb64f46cc978b1c4243e5fb8
Raul Treviño <rtv031.itc@gmail.com>
2016-11-07 10:18:29 +0100

    Main Cycle Created

--- ZorkGameMaster/ZorkGameMaster/ZorkGameMaster.cpp
+++ ZorkGameMaster/ZorkGameMaster/ZorkGameMaster.cpp
@@ -2,10 +2,47 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+using namespace std;
 
 
 int main()
 {
+	char key;
+	string player_input;
+	cout << "Welcome to my Zork Game\n";
+	cout << "--------------\n";
+	
+
+	while (1) {
+		if (_kbhit() != 0) {
+			key = _getch();
+			if (key == '\b') {
+				if (player_input.length() > 0) {
+					player_input.pop_back();
+					cout << '\b';
+					cout << " ";
+					cout << '\b';
+				}
+			}
+			else if (key != '\r') {
+				player_input += key;
+				cout << key;
+				
+			}
+			else {
+				cout << "Tokenize";
+				cout << endl;
+			}
+
+			//Interpretate instructions
+		}
+	}
+
+	cout << "\nThanks for playing,Bye\n";
     return 0;
 }
 
