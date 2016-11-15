0c85c3a140f0528271837b31e4ea4eb8a1bf0b34
Raul Treviño <rtv031.itc@gmail.com>
2016-11-15 15:46:05 +0100

    Sentence tested

--- ZorkGameMaster/ZorkGameMaster/ZorkGameMaster.cpp
+++ ZorkGameMaster/ZorkGameMaster/ZorkGameMaster.cpp
@@ -49,7 +49,9 @@ int main()
 
 				if (args.size() > 0 && args[0]!="") {
 					MyWorld.SendInstrucction(args,ExitFlag);
-					if (ExitFlag) { break; }
+					if (ExitFlag) {
+						break; 
+					}
 				}
 
 				args.clear();
